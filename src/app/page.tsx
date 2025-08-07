8d6b8a6f037df937b4ee9be749d1753a9d221d39
Scheff Chuk <darthusian@gmail.com>
2025-08-07 13:18:25 +0900

    fix: Wrap article-previw component in suspense boundary

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import { LoadingText } from "@/components/ui/loading-text";
 import { ArticlesPreview } from "./article-preview";
-import { unstable_ViewTransition as ViewTransition } from "react";
+import { Suspense, unstable_ViewTransition as ViewTransition } from "react";
 
 export default function HomePage() {
   return (
     <ViewTransition>
-      <ArticlesPreview />
+      <Suspense fallback={<LoadingText />}>
+        <ArticlesPreview />
+      </Suspense>
     </ViewTransition>
   );
 }
